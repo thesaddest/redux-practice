b85615238fc85e1dc53cf38d5b30066f6287f493
thesaddest <youngscarlxrd@yandex.by>
2022-06-01 23:29:54 +0200

    starting flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state,cash)
+
+    console.log(cash)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'app'}>
+          <div style={{display: 'flex'}}>
+              <button>ADD CASH</button>
+              <button>WITHDRAW CASH</button>
+          </div>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {configureStore, createStore} from "@reduxjs/toolkit";
+
+const defaultState = {
+    cash: 0
+}
+
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "ADD_CASH":
+            return {...state, cash: state.cash + action.payload}
+        case "WITHDRAW_CASH":
+            return {...state, cash: state.cash - action.payload}
+        default:
+            return state
+    }
+}
+
+const store = configureStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
